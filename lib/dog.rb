f081b1af195d2af81e102fcee7ae195180da0ef3
Clark Chen <yyunqingchenn@gmail.com>
2019-09-02 03:03:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 # Your code goes here!
 class Dog
-  def initialize(name)
-    @name = name 
-  end 
-    
-  def name
-    @name
+  def name = (dog_name)
+    @name = dog_name
   end
+  
+  def name
+    @name 
+  end
+
+  def bark
+    puts "woof!"
+  end 
 end 
